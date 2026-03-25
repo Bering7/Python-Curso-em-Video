0cab052e63c83320262134bbd7a4f0785872474c
Bering7 <riquemoura.bering@gmail.com>
2026-03-25 16:55:42 -0300

    auto-commit

---
+++ ex109/coin.py
@@ -0,0 +1,3 @@
+def increase(price):
+    increase = price + (price * 10/100)
+    return increase
---
+++ ex109/main.py
@@ -0,0 +1,6 @@
+import coin
+price = float(input('Type the price: '))
+# type_coin = input('Coin to be used: ')
+# print(f'The half of {coin.coin(price)} is {coin.half(price, True)}')
+# print(f'The double of {coin.coin(price)} is {coin.double(price, True)}')
+print(f'Increasing 10% of {price} is {coin.increase(price)}')
