e9e267ed9369592a1d0cd5da0958fe9fb5d0e0a4
Bering7 <riquemoura.bering@gmail.com>
2026-04-27 20:26:42 -0300

    register system

---
+++ ex107/moeda.py
@@ -0,0 +1,15 @@
+def increase(n):
+    i = n + (n * 10/100)
+    return i
+
+def decrease(n):
+    d = n - (n * 10 / 100)
+    return d
+
+def double(n):
+    doub = n * 2
+    return doub
+
+def half(n):
+    hal = n // 2
+    return hal
