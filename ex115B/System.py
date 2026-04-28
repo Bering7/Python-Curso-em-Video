9e8731a07ca829dbadbb6ee1c55eee175e672856
Bering7 <riquemoura.bering@gmail.com>
2026-04-28 17:26:59 -0300

    auto-commit

--- ex115B/System.py
+++ ex115B/System.py
@@ -1,9 +1,18 @@
 from library.utilities import *
+from library.file import *
+
+file = 'ex115B/cursoemvideo.txt'
+if fileExists(file):
+    print('File already exists.')
+else:
+    with open(file, 'w') as file:
+        file.write('')
+    print('File created successfully!')
 
 while True:
     answer = menu(['View Registred Users', 'Register a New User', 'Log Out'])
     if answer == 1:
-        header('Option 1')
+        readFile(file)
     elif answer == 2:
         header('Option 2')
     elif answer == 3:
